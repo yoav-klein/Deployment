dc46fa80e4acf1066773b10b192f6e2b30a3622d
yoav-klein <57894892+yoav-klein@users.noreply.github.com>
2020-11-18 17:20:51 +0200

    Update deployment_script.groovy

--- libraries/fpga/deployment_script.groovy
+++ libraries/fpga/deployment_script.groovy
@@ -3,7 +3,7 @@ def call() {
 	config.files_to_upload.each { file ->
 		
 			powershell script: """
-				echo "Copying ${file} to ${config.copy_to_directory}
+				echo \"Copying ${file} to ${config.copy_to_directory}\"
 				cp ${file} ${config.copy_to_directory}
 			"""
 		
