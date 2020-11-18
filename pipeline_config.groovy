3b3ec82c1ed2209eaa1c36e0d7ca22303271cbd3
yoav-klein <57894892+yoav-klein@users.noreply.github.com>
2020-11-18 16:49:10 +0200

    Update pipeline_config.groovy

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -1,5 +1,9 @@
 
 libraries {
+    fpga {
+        files_to_upload = ["products/prod1", "products/prod2"]
+        copy_to_direcotry = "artifacts"
+    }
     deployment {
         artifact = "BOOT.BIN"
     }
